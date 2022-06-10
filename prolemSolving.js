aa87a110ef365b7ebd4c7e1fde2d4100da9a6d91
Ozan <ozan.yasin97@gmail.com>
2022-06-11 00:17:44 +0300

    Problem Solving Updated

--- prolemSolving.js
+++ prolemSolving.js
@@ -5,3 +5,25 @@
 // - Define what an algorithm is
 // - Devise (planning) a plan to solve algorithms
 // - Compare and contrast problem solving patterns including frequency counters, two pointer problems and divide and conquer
+
+// What is an Algorithm?
+
+// A process or set of steps to accomplish a certain task.
+// Almost everything that you do in programming inlolves some kind of algorithm.
+// It's the foundation for being a successful problem solving and developer
+// Also... INTERVIEWS, INTERVIEWS, and INTERVIEWS!
+
+// How do you Improve?
+
+// 1- Devise (design) a plan for solving problems
+// 2- Master common problem solving patterns.
+
+// ------- First Section: Problem Solving Strategies -------
+
+// A simple, foolproof, magical, miraculous, fail-safe approach! (*NOT REALLY)
+
+// * Understand the Problem
+// * Explore Concrete (somut) Examples
+// * Break It Down
+// *
+// *
