df0bdd65653572c72e6a6dc8ff04c3fa16984ba0
Ozan <ozan.yasin97@gmail.com>
2022-06-14 23:48:49 +0300

    problemSolvingPatterns updated - NEW CHALLANGE ADDED (Multiple Pointers - sumZero)

--- problemSolvingPatterns.js
+++ problemSolvingPatterns.js
@@ -28,7 +28,7 @@
 // Write a function called same, which accepts two arrays. The function should return true if every value in the array has it's corresponding value squared in the second array. The frequency of values must be the same.
 // ==========================================================================
 
-// A NAIVE SOLUTON
+// ----- NAIVE SOLUTON -----
 
 function same1(arr1, arr2) {
   if (arr1.length !== arr2.length) {
@@ -117,3 +117,38 @@ validAnagram("anagrams", "nagaramm");
 // Creating pointers or values that correspond to an index or position and move towards the beginning, end or middle based on a certain condition.
 
 // Very efficient for solving problems with minimal space complexity as well.
+
+// ============================================================================
+// Write a function called sumZero which accepts a sorted array of integers. The function should find the first pair where the sum is 0. Return an array that includes both values that sum to zero or undefined if a pair does not exist.
+// ============================================================================
+
+// ----- NAIVE SOLUTION -----
+
+function sumZero1(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        return [arr[i], arr[j]];
+      }
+    }
+  } // Time Complexity -> O(n^2)
+} // Space Complexity -> O(1)
+
+// ----- REFACTORED SOLUTION -----
+
+function sumZero2(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < rigth) {
+    // It has to be 'left < rigth' due to failsafe scenario.
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  } // Time Complexity -> O(n)
+} // Space Complexity -> O(1)
